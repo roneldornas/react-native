d43984c75ef6f6357afe6c88d167c4e8a3ccd64d
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-16 17:37:15 -0200

    aula 24

--- albums/src/componets/header.js
+++ albums/src/componets/header.js
@@ -3,7 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // Make a component
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+    const { textStyle } = styles;
+
+    return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 25
+    }
+};
 
 // Make component avaiable to other parts of the app
 export default Header;
