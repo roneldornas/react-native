6db5dd105e3ed9920c260e64ff2d41393d1592b0
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-18 21:56:44 -0200

    aula 73

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '', error: '' };
+    state = { email: '', password: '', error: '', loading: false };
 
     onButtonPress() {
         const { email, password } = this.state;
 
-        this.setState({ error: '' });
+        this.setState({ error: '', loading: true });
 
 // Password deve ter no mínimo 6 caracteres ou ocorrerá erro do Firebase
 
@@ -20,6 +20,18 @@ class LoginForm extends Component {
                 this.setState({ error: 'Authentication Failed' });
             });
         });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Log In
+            </Button>
+        );
     }
     
     render() {
@@ -48,9 +60,7 @@ class LoginForm extends Component {
                     {this.state.error}
                 </Text>
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Log In
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
 
             </Card>
