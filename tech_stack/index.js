81932f7df33135f582c26b5147a64d3787f7d8be
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-19 19:58:38 -0200

    aula 84

--- tech_stack/index.js
+++ tech_stack/index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
+import { Provider } from 'react-redux';
+
 
 AppRegistry.registerComponent('tech_stack', () => App);
