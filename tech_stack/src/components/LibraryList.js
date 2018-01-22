7cc000ce90d53cefcec8f78040f6d9f00830aee4
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-22 14:44:40 -0200

    aula 98

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
-import { ListItem } from './ListItem';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
     componentWillMount() {
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         });
-
         this.dataSource = ds.cloneWithRows(this.props.libraries);
     }
 
