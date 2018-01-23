95778f397dd7b800de7f3a269341190909b6aa85
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-23 19:00:40 -0200

    aula 116

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    rendder() {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDJ7DG5rbHdrLDkkbhtWFsdjfCVsEZn3vw',
+            authDomain: 'manager-5246d.firebaseapp.com',
+            databaseURL: 'https://manager-5246d.firebaseio.com',
+            projectId: 'manager-5246d',
+            storageBucket: 'manager-5246d.appspot.com',
+            messagingSenderId: '891744075065'
+        };
+        
+        firebase.initializeApp(config);
+    }
+
+    render() {
         return (
-            <Provider>
+            <Provider store={createStore(reducers)}>
                 <View>
-                    <Text>
-                        Hello!!!
-                    </Text>
+                    <LoginForm />
                 </View>
             </Provider>
         );
