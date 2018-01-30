9a3476dd511987b8aa0c66132ede8d830a1762e3
Ronel Dornas <roneldornas@Guilhermes-MacBook-Pro.local>
2018-01-30 16:21:13 -0200

    curso completado

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -44,8 +44,20 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
         .set({ name, phone, shift })
         .then(() => {
-            dispatch({  type: EMPLOYEE_SAVE_SUCCESS });
-            Actions.employeeList({ type: 'reset' });
+            dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+            Actions.pop({ type: 'reset' });
+        });
+    };
+};
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+        .remove()
+        .then(() => {
+            Actions.main({ type: 'reset' });
         });
     };
 };
